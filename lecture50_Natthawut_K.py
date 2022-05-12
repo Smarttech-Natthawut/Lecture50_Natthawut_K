dd41c0eba5d8961ec227861418912d9ea02d0198
Smarttech-Natthawut <nattawut.smarttech@gmail.com>
2022-05-12 14:19:45 +0700

    Create lecture50_Natthawut_K.py

---
+++ lecture50_Natthawut_K.py
@@ -0,0 +1,15 @@
+x = 100
+y = 50
+def addNumber(x ,y):
+    print (x + y)
+def decreaseNumber(x ,y):
+    print (x - y)
+def multiplyNumber(x ,y):
+    print (x * y)
+def divideNumber(x ,y):
+    print (x / y)
+
+addNumber(x , y)
+decreaseNumber(x ,y)
+multiplyNumber(x ,y)
+divideNumber(x ,y)
